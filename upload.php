a147d675a1546bf3d2e3019db51c61d3e97248a3
e29ckg <e29ckg@gmail.com>
2024-05-20 16:43:32 +0700

    Initial commit

---
+++ upload.php
@@ -0,0 +1,259 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Content-Type, Accept");
+header("Content-Type: application/json; charset=utf-8");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
+        $filename = $_FILES['file']['tmp_name'];
+
+        // ตรวจสอบว่าเป็นไฟล์ .txt
+        $fileinfo = pathinfo($_FILES['file']['name']);
+        $name_mode = substr($_FILES['file']['name'], 0, 3); // DCT   MCL
+        
+        
+        if (isset($fileinfo['extension']) && strtolower($fileinfo['extension']) == 'txt') {
+            // อ่านเนื้อหาภายในไฟล์
+            if ($name_mode !== "DCT" && $name_mode !== "MCL") {
+                http_response_code(200);
+                $data = array(
+                    'status' => 'error',
+                    'message' => 'ไม่พบข้อมูล ',
+                );
+                echo json_encode($data);
+                exit();
+            }
+            
+            $content = file_get_contents($filename, FILE_IGNORE_NEW_LINES);
+            $text_utf8 = iconv(mb_detect_encoding($content, mb_detect_order(), true), "UTF-8", $content);
+
+            $data = explode("\n", $text_utf8);
+            $arr_lines = array();
+            foreach ($data as $line) {
+                if (strpos($line, " ") !== false) {
+                    $arr_lines[] = $line;
+                }
+            }            
+            
+            $datas = array();
+            $header = getHeder($arr_lines[0]);
+
+
+
+            foreach ($arr_lines as $index => $line) {
+                // แยกข้อมูลในบรรทัด
+                $data = explode(" ", $line);
+                $arr_data = array();
+
+                foreach ($data as $value) {
+                    $trimmed_value = trim($value); // ลบช่องว่างที่ติดอยู่ด้านหน้าและด้านหลังของข้อความ
+                    if ($trimmed_value !== "") { // ตรวจสอบว่าข้อมูลไม่เป็นช่องว่าง
+                        $arr_data[] = $trimmed_value;
+                    }
+                }
+
+                if($index % 2 === 1) {
+                    $datas[] = getDatas($arr_data, $header);
+                }
+                
+            }
+        } else {
+            http_response_code(200);
+            $data = array(
+                'status' => 'error',
+                'message' => 'กรุณาอัปโหลดไฟล์ .txt เท่านั้น'
+            );
+            echo json_encode($data);
+            exit();
+        }
+    } else {
+        http_response_code(200);
+        $data = array(
+            'status' => 'error',
+            'message' => 'เกิดข้อผิดพลาดในการอัปโหลดไฟล์'
+        );
+        echo json_encode($data);
+        exit();
+    }
+} else {
+    http_response_code(200);
+    $data = array(
+        'status' => 'error',
+        'message' => 'วิธีการร้องขอไม่ถูกต้อง'
+    );
+    echo json_encode($data);
+    exit();
+}
+
+// ข้อมูลที่ต้องการส่งกลับ
+http_response_code(200);
+$data = array(
+    'status' => 'success',
+    'header' => $header,
+    'datas' => $datas,
+    'message' => 'Data received successfully'
+);
+echo json_encode($data);
+exit;
+
+
+function getHeder($arr_lines){
+    $version = null;
+
+    $data = $arr_lines;
+    $data = explode(" ", $data);
+
+    $arr_data = array();    
+    
+    foreach ($data as $value) {
+        $trimmed_value = trim($value); // ลบช่องว่างที่ติดอยู่ด้านหน้าและด้านหลังของข้อความ
+        if ($trimmed_value !== "") { // ตรวจสอบว่าข้อมูลไม่เป็นช่องว่าง
+            $arr_data[] = $trimmed_value;
+        }
+    }
+    
+    $version = substr($arr_data[0], -3);
+    $coce_pf = substr($arr_data[0], 0,4);
+    $code = "";
+    $debit_account = "";
+    $court = "ศาลจังหวัดกาญจนบุรี";
+    $amount = "";
+    $date_now = "";
+    
+    
+    
+    if ($version === "130" && $coce_pf === "HDCT") {
+        $code = $arr_data[0];
+        $debit_account = $arr_data[2];
+        $court = $arr_data[5];        
+        $amount = $arr_data[3];
+        $amount = intval($amount);
+        $amount = number_format($amount / 100, 2, '.', ',');
+        $date_now = $arr_data[4];
+        $date = DateTime::createFromFormat('ymd', $date_now);
+        $date_now = $date->format('Y-m-d');
+
+    }elseif ($version === '130' && $coce_pf === "HMCL") {
+        $code = $arr_data[0];
+        $amount = $arr_data[2];
+        $amount = substr($amount, 18, 19);
+        $amount = intval($amount);
+        $amount = number_format($amount, 2, '.', ',');
+        $date_now = $arr_data[1];
+        $date = DateTime::createFromFormat('d-m-Y', $date_now);
+        $date_now = $date->format('Y-m-d');
+
+    }elseif ($version === '129') {
+        $code = $arr_data[0];
+        $amount = $arr_data[2];
+        $amount = substr($amount, 18, 19);
+        $amount = intval($amount);
+        $amount = number_format($amount, 2, '.', ',');
+        $date_now = $arr_data[1];
+        $date = DateTime::createFromFormat('d-m-Y', $date_now);
+        $date_now = $date->format('Y-m-d');
+
+    }
+
+    return [
+        "status"=> "success",
+        "version"=> $version,
+        'coce_pf' => $coce_pf,
+        'code' => $code,
+        'debit_account'=> $debit_account,
+        'amount'=> $amount,
+        'date_now'=> $date_now,
+        'court'=> $court,
+        // 'all'=> $arr_data,  
+    ];    
+
+};
+
+function getDatas($arr_lines,$header){
+    $code_pf = $header["coce_pf"];
+    $version = $header["version"];
+
+    $code = "";
+    $account = "";
+    $amount = "";
+    $date_now = $header["date_now"];
+    $vendor_name = "";
+    $effective_date = $header["date_now"];
+    $bene_ref = "";
+    $personal_id = "";
+
+    if ($version === "130" && $code_pf === "HDCT") {
+        $code = $arr_lines[0];
+        $account = $arr_lines[1];
+        $amount = $arr_lines[2];
+        $amount = intval($amount);
+        $amount = number_format($amount, 2, '.', ',');
+        $date_now = $arr_lines[3];
+        $date = DateTime::createFromFormat('ymd', $date_now);
+        $date_now = $date->format('Y-m-d');
+        $vendor_name = $arr_lines[4].' ' .$arr_lines[5];
+        $effective_date =  $arr_lines[6];
+        $effective_date = substr($effective_date, 0, 6);
+        $effective_date = DateTime::createFromFormat('ymd', $effective_date);
+        $effective_date = $effective_date->format('Y-m-d');
+        $bene_ref = $arr_lines[6];
+        $bene_ref = substr($bene_ref, 9);
+        $personal_id = substr($arr_lines[8], 0, 13);        
+
+    } else if ($version === "130"&& $code_pf === "HMCL"){
+        $code = $arr_lines[0];
+        for ($i = 7; $i < count($arr_lines); $i++) {
+            if (strlen($arr_lines[$i]) === 30) {
+                $account = $arr_lines[$i];    
+                $bene_ref = $arr_lines[$i];    
+            }    
+            if (strlen($arr_lines[$i]) === 20) {
+                $personal_id = $arr_lines[$i]; 
+            }    
+        }
+        $account = substr($account, 10, 10);
+        $bene_ref = substr($bene_ref, 20);
+        $amount = $arr_lines[1];
+        $amount = substr($amount,0,13);
+        $amount = intval($amount);
+        $amount = number_format($amount, 2, '.', ',');
+        $vendor_name = substr($arr_lines[1],13). ' '.$arr_lines[2];
+        $personal_id = substr($personal_id, 0, 13);
+
+    }else if ($version === "129"&& $code_pf === "HMCL"){
+        $code = $arr_lines[0];
+        for ($i = 7; $i < count($arr_lines); $i++) {
+            if (strlen($arr_lines[$i]) === 30) {
+                $account = $arr_lines[$i];    
+                $bene_ref = $arr_lines[$i];    
+            }    
+            if (strlen($arr_lines[$i]) === 20 || strlen($arr_lines[$i]) === 16 ) {
+                $personal_id = $arr_lines[$i]; 
+            }    
+        }
+        $account = substr($account, 10, 10);
+        $bene_ref = substr($bene_ref, 20);
+        $amount = $arr_lines[1];
+        $amount = substr($amount,0,13);
+        $amount = intval($amount);
+        $amount = number_format($amount, 2, '.', ',');
+        $vendor_name = substr($arr_lines[1],13). ' '.$arr_lines[2];
+        $personal_id = substr($personal_id, 0, 13);
+    }
+
+    return  [
+        "coce_pf" =>$code_pf,
+        "version" =>$version,
+        "code" => $code,
+        "account" => $account,
+        "amount" => $amount,
+        "date_now" => $date_now,
+        "vendor_name" => $vendor_name,
+        "effective_date" => $effective_date,
+        "bene_ref" => $bene_ref,
+        "personal_id" => $personal_id,
+        "datas" =>$arr_lines,
+    ];
+};
